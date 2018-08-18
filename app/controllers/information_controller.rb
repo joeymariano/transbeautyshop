e294094912970bf38824cc2391520a2bbb7d1fd3
nmlstyl <joey.michalina.mariano@gmail.com>
2018-08-18 18:22:09 -0400

    setup routes

---
+++ app/controllers/information_controller.rb
@@ -0,0 +1,13 @@
+class InformationController < ApplicationController
+  def about
+  end
+
+  def services
+  end
+
+  def faqs
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "welcome#index"
+  get '/about', to: 'information#about'
+  get '/contact', to: 'information#contact'
+  get '/faqs', to: 'information#faqs'
+  get '/services', to: 'information#services'
 end
