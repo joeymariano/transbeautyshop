e8ba0ca99d13e615197eaa05472acb98f91a60e9
nmlstyl <joey.michalina.mariano@gmail.com>
2018-09-05 01:52:55 -0400

    can send email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_USER'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
