6bfe97f764dbe9a6022aae5bcc349f7d2ada697d
nmlstyl <joey.michalina.mariano@gmail.com>
2018-09-07 19:56:01 -0400

    changed environment file to config Action Mailer hopefully soon

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# ActionMailer::Base.smtp_settings = {
-#   :port           => 587,
-#   :address        => "smtp.mailgun.org",
-#   :domain         => ENV['MAILGUN_DOMAIN'],
-#   :user_name      => ENV['MAILGUN_LOGIN'],
-#   :password       => ENV['MAILGUN_PASSWORD'],
-#   :authentication => :plain
-# }
+ActionMailer::Base.smtp_settings = {
+  :port           => 587,
+  :address        => "smtp.mailgun.org",
+  :domain         => ENV['MAILGUN_DOMAIN'],
+  :user_name      => ENV['MAILGUN_LOGIN'],
+  :password       => ENV['MAILGUN_PASSWORD'],
+  :authentication => :plain
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for mailgun
-  ActionMailer::Base.smtp_settings = {
+config.action_mailer.smtp_settings = {
     :port           => 587,
     :address        => "smtp.mailgun.org",
     :domain         => ENV['MAILGUN_DOMAIN'],
