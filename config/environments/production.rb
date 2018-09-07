4a800ba43b1e1f7586dd268466154fe41d8da14c
nmlstyl <joey.michalina.mariano@gmail.com>
2018-09-07 16:50:13 -0400

    configuring mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for mailgun
+  ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => "smtp.mailgun.org",
+    :domain         => ENV['domain'],
+    :user_name      => ENV['username'],
+    :password       => ENV['password'],
+    :authentication => :plain,
+  }}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
