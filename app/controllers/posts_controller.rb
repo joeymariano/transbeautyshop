cf107deefea9cd43b0c1bfccfd9af46a7d34bd78
joeymariano <joey.michalina.mariano@gmail.com>
2019-10-10 01:57:17 -0400

    can delete a post now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	layout "user_layout"
+	# before_action: 
 
 	def create
 		post = Post.new(post_params)
@@ -25,6 +26,18 @@ class PostsController < ApplicationController
 		end
 		redirect_to edit_user_post_path(@user, @post)
 	end
+
+	def destroy
+		binding.pry
+		if is_logged_in?
+			post = Post.find(params['id'])
+			user = User.find(params['user_id'])
+			post.delete
+			redirect_to user_path(user)
+		else
+			redirect_to '/'
+		end
+	end 
 
 	def is_logged_in?
 		user = User.find(params[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
 
 
  	resources :users, only: [:show] do
- 		resources :posts, only: [:create, :edit, :update]
+ 		resources :posts, only: [:create, :edit, :update, :destroy]
  	end
 end
